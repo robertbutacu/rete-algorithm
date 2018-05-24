5dc618d4f54af40c6bd4d03cf15ae634f84f4dac
robertbutacu <robert.butacu@yahoo.com>
2018-05-24 18:41:57 +0300

    initial code for uploading file

--- backend/rete-algorithm/src/server/Server.py
+++ backend/rete-algorithm/src/server/Server.py
@@ -1,10 +1,12 @@
-from flask import app, Flask, request
+import os
+from __builtin__ import file
+from flask import app, Flask, request, flash, url_for
+from werkzeug.utils import redirect, secure_filename
 
 from src.services.ServerServices import build_network
 
 UPLOAD_FOLDER = 'E:\\Projects\\rete-algorithm\\uploads'
 ALLOWED_EXTENSIONS = {'clp'}
-
 
 app = Flask(__name__)
 
@@ -21,12 +23,25 @@ def graph_for_code():
     print(code)
 
     return "welcome"
-    #return build_network(code, False)
+    # return build_network(code, False)
 
 
 @app.route('/file', methods=['POST'])
 def graph_from_file():
-    return None
+    def allowed_file(file):
+        return '.' in file and \
+               file.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+    if 'file' not in request.files:
+        flash('No file part')
+        return redirect(request.url)
+    if file.filename == '':
+        flash('No selected file')
+        return redirect(request.url)
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        return redirect(url_for('uploaded_file', filename=filename))
 
 
 if __name__ == "__main__":
