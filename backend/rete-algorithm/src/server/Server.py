d4e6171270f28e56b65e1ca2a7dec1931f6626bb
robertbutacu <robert.butacu@yahoo.com>
2018-05-24 18:18:59 +0300

    testing endpoint

--- backend/rete-algorithm/src/server/Server.py
+++ backend/rete-algorithm/src/server/Server.py
@@ -1,4 +1,11 @@
-import Flask as Flask
+from flask import app, Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return "Hello World!"
 
 if __name__ == "__main__":
-    flask = Flask(__name__)
+    app.run()
