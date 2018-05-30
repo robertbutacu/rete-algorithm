e591aee63d72f76e834cd9ece279c1b45ca98e2b
robertbutacu <robertbutacu@gmail.com>
2018-05-30 13:38:56 +0300

    build lower levels for nodes -> almost finished

--- backend/rete-algorithm/src/services/mappers/ResponseNode.py
+++ backend/rete-algorithm/src/services/mappers/ResponseNode.py
@@ -1,5 +1,6 @@
 from flask import jsonify
 
+from src.rete.algorithm.Nodes import DummyJoinNode, JoinNode, PNode
 from src.rete.algorithm.Text import Text
 
 
@@ -43,17 +44,42 @@ class ResponseNode:
         self.__activations = value
 
     def to_dict(self):
+        def build_lower_levels(node):
+            result = {}
+            if isinstance(node, DummyJoinNode):
+                result["text"] = "D.JOIN"
+                alpha_memory_dict = {"text": str(node.alpha_memory)}
+                alpha_memory_children = []
+                for c in node.children:
+                    alpha_memory_children.append(build_lower_levels(c))
+                    alpha_memory_dict["children"] = alpha_memory_children
+                result["children"] = [alpha_memory_dict]
+
+            if isinstance(node, JoinNode):
+                result["text"] = "JOIN"
+                alpha_memory_dict = {"text": str(node.alpha_memory)}
+                alpha_memory_children = []
+                for c in node.children:
+                    alpha_memory_children.append(build_lower_levels(c))
+                    alpha_memory_dict["children"] = alpha_memory_children
+                result["children"] = [alpha_memory_dict]
+
+            if isinstance(node, PNode):
+                result["text"] = node.name
+                result["children"] = []
+
+            return result
+
         dict_children = []
 
         for child in self.__children:
             dict_children.append(child.to_dict())
 
-        if self.__alpha_memory_node is not None or self.__alpha_memory_node.__str__() != ' Empty Alpha Memory':
-            if self.__alpha_memory_node:
-                for c in self.__alpha_memory_node.children:
-                    print(c.alpha_memory.__str__)
-                    print(c.alpha_memory.children)
-            dict_children.append({"text": str(self.__alpha_memory_node), "children": []})
+        if self.__alpha_memory_node:
+            children = []
+            for c in self.__alpha_memory_node.children:
+                children.append(build_lower_levels(c))
+            dict_children.append({"text": str(self.__alpha_memory_node), "children": children})
 
 
         result_dict = {"text": self.__text.to_dict(),
